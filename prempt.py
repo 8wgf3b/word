a374ab5e04f6683fcd5af51787fbd0a135705375
8wgf3b <karnabalaj@gmail.com>
2022-11-01 18:40:22 +0530

    scrabble init

--- prempt.py
+++ prempt.py
@@ -1,15 +1,22 @@
 import string
 import numpy as np
+import pickle
+from collections import Counter
 
 
 L = string.ascii_lowercase
 Ref = {v: k for k, v in enumerate(L)}
 
 with open('words_alpha.txt', 'r') as f:
-    content = [x.rstrip() for x in f] 
+    content = [x.rstrip() for x in f]
+    ctr = {x: Counter(x) for x in content}
 
     MAT = np.zeros((26, 32))
 
     for w in content:
         for k, l in enumerate(w):
             MAT[Ref[l]][k] += 1
+
+def matches(x):
+    C = Counter(x)
+    return [i for i in content if ctr[i] == ctr[i] & C]
--- warudo.py
+++ warudo.py
@@ -1,12 +1,12 @@
 import streamlit as st
-from prempt import MAT, Ref, L
+from prempt import MAT, Ref, L, matches
 
 
 """
 # Letter frequency
 """
 
-LT, PT = st.tabs(["By Letter", "By Position"])
+LT, PT, AP = st.tabs(["By Letter", "By Position", "Sub-words"])
 
 with LT:
     LSL = st.select_slider('select the letter', options=L)
@@ -15,4 +15,10 @@ with LT:
 with PT:
     PSL = st.slider('select the position', 0, 32)
     st.bar_chart(MAT[:, PSL])
+
+with AP:
+    inp = st.text_input("Word","aa")
+    ws = matches(inp)
+    st.header(f"There are {len(ws)} words")
+    st.write("\n".join(f"* {x}" for x in ws))
 
